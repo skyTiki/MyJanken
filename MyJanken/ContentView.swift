fea073fd51d4a2866b81bed03fafcae0f6014496
市毛隆俊 <ichigetakatoshi@ichigetakatoshinoMacBook-Air.local>
2021-06-06 11:58:57 +0900

    inital commit

---
+++ MyJanken/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyJanken
+//
+//  Created by 市毛隆俊 on 2021/06/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyJanken/MyJankenApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyJankenApp.swift
+//  MyJanken
+//
+//  Created by 市毛隆俊 on 2021/06/06.
+//
+
+import SwiftUI
+
+@main
+struct MyJankenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
